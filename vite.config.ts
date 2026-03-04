402b36828f93b50ee9fd8bda5bd52ca9fa92cfda
Jonathan Brown <jbrown@acuity.network>
2026-03-04 08:52:27 +0700

    Deploy to GitHub pages.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  base: "/solana-payment-scheduler-dapp/", // e.g. "/scheduled-transfer-ui/"
   plugins: [
     react(),
     nodePolyfills({
